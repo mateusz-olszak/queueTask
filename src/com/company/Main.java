6d187290521a84a09d6801b803aac3586f53d96b
OlBeds <mariuszolszak@192.168.1.30>
2021-05-30 15:45:28 +0200

    Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,77 +1,161 @@
 package com.company;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        Keypad keypad = new Keypad();
 
-        Product bigMac = new Product("BigMac", 16.99);
-        Product cheeseburger = new Product("Cheeseburger", 4.99);
-        Product fries = new Product("Fries", 4.99);
-        Product shake = new Product("Shake", 6.50);
-        Product mcFlurry = new Product("McFlurry", 5.99);
-        Product mcHotDog = new Product("McHotDog", 14.99);
-        Customer customer = null;
-        for(int i=0; i<5; i++){
-            customer = new Customer(i);
-        }
+        keypad.welcomeCustomer();
 
-//        Customer customer1 = new Customer(1);
-//        Customer customer2 = new Customer(2);
-//        Customer customer3 = new Customer(3);
 
-        List<Product> products1 = new LinkedList<>();
-        products1.add(bigMac);
-        products1.add(fries);
-
-        List<Product> products2 = new LinkedList<>();
-        products2.add(shake);
-        products2.add(mcFlurry);
-
-        List<Product> products3 = new LinkedList<>();
-        products3.add(cheeseburger);
-        products3.add(shake);
-        products3.add(mcHotDog);
-
-        List<Product> products4 = new LinkedList<>();
-        products4.add(shake);
-        products4.add(fries);
-        products4.add(fries);
-
-        Order order1 = new Order(customer, products2);
-        Order order2 = new Order(customer, products3);
-        Order order3 = new Order(customer, products1);
-        Order order4 = new Order(customer, products4);
-
-        Deque<Order> orders = new ArrayDeque<>();
-
-        orders.offer(order1);
-        orders.offer(order2);
-        orders.offer(order3);
-        orders.offer(order4);
-
-        orders.forEach(x -> System.out.println(x.toString()));
 
     }
 }
 
+class Keypad{
+
+    private Scanner input = new Scanner(System.in);
+    private Product product = new Product();
+    private Cart cart = new Cart();
+
+    public void welcomeCustomer(){
+
+        System.out.println("Hello in our brand new restaurant...\nPlease select an option below: ");
+        System.out.print("menu - 1 | cart - 2 | special offers - 3\n: ");
+        char choice = input.nextLine().charAt(0);
+
+       if (validateWelcomeChoice(choice)) printOptions(choice);
+       else welcomeCustomer();
+
+    }
+
+    public void printOptions(char choice){
+
+        switch (choice){
+            case '1':
+                System.out.println(product.listMenu());
+                System.out.println("-------------");
+                System.out.print("Add to cart[1-6] or quit['q']: ");
+                char optionMenu = input.nextLine().charAt(0);
+                String productName;
+                BigDecimal productPrice;
+
+
+                if (validateMenuChoice(optionMenu) && optionMenu != 'q'){
+                    switch (optionMenu){
+                        case '1':
+                                System.out.println("You picked cheeseburger");
+                                productName = product.getCheeseburger();
+                                productPrice = product.getCheeseburger_price();
+                                cart.setProductsName(productName);
+                                cart.setProductsFinalPrice(productPrice);
+                                printOptions(choice);
+                                break;
+                            case '2':
+                                System.out.println("You picked fries");
+                                productName = product.getFries();
+                                productPrice = product.getFries_price();
+                                cart.setProductsName(productName);
+                                cart.setProductsFinalPrice(productPrice);
+                                printOptions(choice);
+                                break;
+                            case '3':
+                                System.out.println("You picked shake");
+                                productName = product.getShake();
+                                productPrice = product.getShake_price();
+                                cart.setProductsName(productName);
+                                cart.setProductsFinalPrice(productPrice);
+                                printOptions(choice);
+                                break;
+                            case '4':
+                                System.out.println("You picked coke");
+                                productName = product.getCoke();
+                                productPrice = product.getCoke_price();
+                                cart.setProductsName(productName);
+                                cart.setProductsFinalPrice(productPrice);
+                                printOptions(choice);
+                                break;
+                            case '5':
+                                System.out.println("You picked taco");
+                                productName = product.getTaco();
+                                productPrice = product.getTaco_price();
+                                cart.setProductsName(productName);
+                                cart.setProductsFinalPrice(productPrice);
+                                printOptions(choice);
+                                break;
+                            case '6':
+                                System.out.println("You picked icecream");
+                                productName = product.getIcecream();
+                                productPrice = product.getIcecream_price();
+                                cart.setProductsName(productName);
+                                cart.setProductsFinalPrice(productPrice);
+                                printOptions(choice);
+                                break;
+
+                            default:
+                                printOptions(choice);
+                                break;
+
+                    }
+                }
+                else welcomeCustomer();
+                break;
+            case '2':
+                System.out.println("---------------------");
+                System.out.println(cart.toString());
+                System.out.println("Make an order['o'] or quit['q']: ");
+                optionMenu = input.nextLine().charAt(0);
+                switch (optionMenu){
+                    case 'o':
+                        Customer customer = new Customer();
+                        Order order = new Order(customer,cart);
+                        System.out.println("Your order:\n" + order);
+                        break;
+                    case 'q':
+                        welcomeCustomer();
+                        break;
+                    default:
+                        printOptions(choice);
+                        break;
+                }
+                break;
+            case '3':
+                break;
+        }
+    }
+
+    public boolean validateWelcomeChoice(char choice){
+        if(choice == '1' || choice == '2' || choice == '3') return true;
+        else return false;
+    }
+
+    public boolean validateMenuChoice(char choice){
+        if(choice == '1' || choice == '2' || choice == '3' || choice == '4' || choice == '5' || choice == '6') return true;
+        else return false;
+    }
+
+}
+
+
 class Order{
 
     private Customer customer;
-    private List<Product> product;
+    private Cart cart;
 
-    public Order(Customer customer, List<Product> product) {
+    public Order(Customer customer, Cart cart) {
         this.customer = customer;
-        this.product = product;
+        this.cart = cart;
     }
 
     @Override
     public String toString() {
         return "Order{" +
                 "customer=" + customer +
-                ", product=" + product +
+                ", cart=" + cart +
                 '}';
     }
 }
@@ -79,60 +163,125 @@ class Order{
 class Customer{
 
     private int recipeNumber;
+    private final Random random = new Random();
 
-    public Customer(int recipeNumber) {
-        this.recipeNumber = recipeNumber;
-    }
+    public int generateRecipeNumber(){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=0; i<15; i++){
+            int num = random.nextInt(100)+1;
+            if (!list.contains(num)) list.add(num);
+        }
+        recipeNumber = random.nextInt(list.size());
 
-    public int getRecipeNumber() {
         return recipeNumber;
     }
 
     @Override
     public String toString() {
         return "Customer{" +
-                "recipeNumber=" + recipeNumber +
+                "recipeNumber=" + generateRecipeNumber() +
                 '}';
     }
+}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return recipeNumber == customer.recipeNumber;
+class Cart{
+    private List<String> productsName = new LinkedList<>();
+    private List<BigDecimal> productsPrice = new LinkedList<>();
+    private BigDecimal totalPrice = new BigDecimal(0);
+    private BigDecimal sum = getTotalPrice();
+
+    public void setProductsName(String productName) {
+        this.productsName.add(productName);
+    }
+
+    public void setProductsFinalPrice(BigDecimal productsFinalPrice) {
+        this.productsPrice.add(productsFinalPrice);
+    }
+
+    public List<String> getProductNames() {
+        return productsName;
+    }
+
+    public List<BigDecimal> getProductsPrice() {
+        return productsPrice;
+    }
+
+    public BigDecimal getTotalPrice(){
+        for (BigDecimal x : productsPrice){
+            totalPrice = new BigDecimal(x.longValue());
+        }
+        return totalPrice;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(recipeNumber);
+    public String toString() {
+        return "Your cart contains: " + productsName +
+                "\nTotal price: " + productsPrice + " = " + sum;
     }
 }
 
 class Product{
 
-    private final String productName;
-    private final double price;
+    private final String cheeseburger = "cheeseburger";
+    private final String fries = "fries";
+    private final String shake = "shake";
+    private final String coke = "coke";
+    private final String taco = "taco";
+    private final String icecream = "icecream";
 
-    public Product(String productName, double price) {
-        this.productName = productName;
-        this.price = price;
+    private final BigDecimal cheeseburger_price = BigDecimal.valueOf(4.99);
+    private final BigDecimal fries_price = BigDecimal.valueOf(4.99);
+    private final BigDecimal shake_price = BigDecimal.valueOf(5.50);
+    private final BigDecimal coke_price = BigDecimal.valueOf(3.99);
+    private final BigDecimal taco_price = BigDecimal.valueOf(8.99);
+    private final BigDecimal icecream_price = BigDecimal.valueOf(5.99);
+
+    public String getCheeseburger() {
+        return cheeseburger;
     }
 
-    public String getProductName() {
-        return productName;
+    public String getFries() {
+        return fries;
     }
 
-    public double getPrice() {
-        return price;
+    public String getShake() {
+        return shake;
     }
 
-    @Override
-    public String toString() {
-        return "Product{" +
-                "productName='" + productName + '\'' +
-                ", price=" + price +
-                '}';
+    public String getCoke() {
+        return coke;
+    }
+
+    public String getTaco() {
+        return taco;
+    }
+
+    public String getIcecream() {
+        return icecream;
+    }
+
+    public BigDecimal getCheeseburger_price() {
+        return cheeseburger_price;
+    }
+
+    public BigDecimal getFries_price() {
+        return fries_price;
+    }
+
+    public BigDecimal getShake_price() {
+        return shake_price;
+    }
+
+    public BigDecimal getCoke_price() {
+        return coke_price;
+    }
+
+    public BigDecimal getTaco_price() {
+        return taco_price;
+    }
+
+    public BigDecimal getIcecream_price() {
+        return icecream_price;
     }
 
     @Override
@@ -140,11 +289,21 @@ class Product{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return Double.compare(product.price, price) == 0 && Objects.equals(productName, product.productName);
+        return Objects.equals(cheeseburger, product.cheeseburger) && Objects.equals(fries, product.fries) && Objects.equals(shake, product.shake) && Objects.equals(coke, product.coke) && Objects.equals(taco, product.taco) && Objects.equals(icecream, product.icecream);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(productName, price);
+        return Objects.hash(cheeseburger, fries, shake, coke, taco, icecream);
     }
+
+    public String listMenu(){
+        return "1. " + cheeseburger + " - " + cheeseburger_price +
+                "\n2. " + fries + " - " + fries_price +
+                "\n3. " + shake + " - " + shake_price +
+                "\n4. " + coke + " - " + coke_price +
+                "\n5. " + taco + " - " + taco_price +
+                "\n6. " + icecream + " - " + icecream_price;
+    }
+
 }
